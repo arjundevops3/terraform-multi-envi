9f41ffa95e0db300827c1a7cbd42ff72d6f83acb
arjun <arjun3458@gmail.com>
2024-02-08 19:32:57 +0530

    root user

---
+++ tfvars/dev/backend.tf
@@ -0,0 +1,4 @@
+bucket = "daws76s-st-dev"
+key    = "multienv"
+region = "us-east-1"
+dynamodb_table = "daws76s-lock-dev"
---
+++ tfvars/prod/backend.tf
@@ -0,0 +1,4 @@
+bucket = "daws76s-st-prod"
+key    = "multienv"
+region = "us-east-1"
+dynamodb_table = "daws76s-lock-prod"
